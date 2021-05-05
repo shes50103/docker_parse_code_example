e5df3245bbe3f3df7e2a1cf0d48d9dbff286f4e2
shes50103 <shes50103@gmail.com>
2021-05-05 15:41:30 +0800

    Init

---
+++ docker.rb
@@ -0,0 +1,28 @@
+# Step 1
+# Parse code directly
+puts "Current ruby version #{RUBY_VERSION}"
+p `docker run ruby:2.7.1 ruby -e 'puts RUBY_VERSION'`
+
+
+# Step 2
+# Parse code from file
+
+user_input = '
+class Cat
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    "My name is #{@name}"
+  end
+end
+
+ c1 = Cat.new("Kitty")
+ p c1.name'
+
+out_file = File.new("out.txt", "w")
+out_file.write user_input
+out_file.close
+
+p `docker run -v ~/Desktop/astro/7/out.txt:/mnt/mydata ruby:2.7.1 ruby /mnt/mydata`
